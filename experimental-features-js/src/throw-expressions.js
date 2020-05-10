9b19ca451cad53eb3d252155d93dbb738efc706c
Joel Humberto Gomez Paredes <dezkareid@joels-mbp.gdl.mex.ibm.com>
2020-05-10 03:18:29 -0500

    removed unnecessary semicolons

--- experimental-features-js/src/throw-expressions.js
+++ experimental-features-js/src/throw-expressions.js
@@ -1,8 +1,8 @@
 export function updateUser(user = throw new Error('user is required!')) {
   return true;
-};
+}
 
 export function doOperation(isValidOperation) {
   isValidOperation || throw new Error('validation fail');
   return true;
-};
+}
